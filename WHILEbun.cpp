ef9965f11f6b142020c975ecda7d770ca597291a
sola-akiduki <centos_6@yahoo.co.jp>
2016-12-04 02:27:26 +0900

    Add files via upload

---
+++ WHILEbun.cpp
@@ -0,0 +1,23 @@
+#include <iostream>
+using namespace std;
+
+int main()
+{
+	int num = 1;
+	while(1){
+
+		if(num==100){
+			cout << num << "回目です\n";
+			cout << "終了します\n";
+			break;
+		}
+	cout << num << "回目です\n";
+	num++;
+
+	}
+
+
+	return 0;
+
+
+}
