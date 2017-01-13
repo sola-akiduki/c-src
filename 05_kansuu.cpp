7e56dc02c67e70f81bec6187b9e717828905b831
sola-akiduki <centos_6@yahoo.co.jp>
2017-01-14 02:19:41 +0900

    Create 05_kansuu.cpp

---
+++ 05_kansuu.cpp
@@ -0,0 +1,23 @@
+#include <iostream>
+using namespace std;
+
+void num();
+
+int main()
+{
+    int a = 5;
+    cout << a << endl;
+
+    cout << "test" << endl;
+
+    num();
+
+    return 0;
+}
+
+void num()
+{
+
+    int b = 10;
+    cout << b << endl;
+}
